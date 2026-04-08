155cd00e20702f94123472f95e4c89b1042afc05
RomanDonw <romandonw628@gmail.com>
2026-04-08 13:03:48 +1000

    added WSA init implementation

--- include/libsocket.h
+++ include/libsocket.h
@@ -6,6 +6,22 @@
     {
 #endif
 
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64)
+    #define OS_WINDOWS
+#endif
+
+#ifdef OS_WINDOWS
+    #include <winsock2.h>
+
+    typedef SOCKET Socket;
+    #define InvalidSocket INVALID_SOCKET;
+#else
+    #include <sys/socket.h>
+
+    typedef int Socket;
+    #define InvalidSocket -1
+#endif
+
 
 
 #ifdef __cplusplus
--- src/libsocket.c
+++ src/libsocket.c
@@ -1,1 +1,20 @@
 #include "libsocket.h"
+
+#include <stdlib.h>
+
+#ifdef OS_WINDOWS
+    void __attribute__((constructor(101))) init()
+    {
+        const WORD version = MAKEWORD(2, 2);
+
+        WSADATA data;
+        if (WSAStartup(version, &data)) abort();
+
+        if (data.wVersion != version) { WSACleanup(); abort(); }
+    }
+
+    void __attribute__((destructor)) cleanup()
+    {
+        WSACleanup();
+    }
+#endif
