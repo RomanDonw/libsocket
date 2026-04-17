b39e386440598424c96518d7daa453a915dea2c1
RomanDonw <romandonw628@gmail.com>
2026-04-17 19:04:42 +1000

    moved POSIX-specific includes from util.h header to util.c source file

--- src/util.c
+++ src/util.c
@@ -2,6 +2,11 @@
 
 #include "init.h"
 
+#ifndef OS_WINDOWS
+    #include <unistd.h>
+    #include <arpa/inet.h>
+#endif
+
 int fillsockaddrstruct(struct sockaddr *out_sockaddr, SocketAddressFamily af, const char *addr, unsigned short port)
 {
     ENSURE_INIT;
