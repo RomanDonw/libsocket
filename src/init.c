1df1f049c2c745a24ff8710c4e40546495827e0e
RomanDonw <romandonw628@gmail.com>
2026-04-17 20:50:36 +1000

    added error messages on WinSock init

--- src/init.c
+++ src/init.c
@@ -5,6 +5,7 @@ volatile void *___ = NULL;
 
 #ifdef OS_WINDOWS
     #include <stdbool.h>
+    #include <stdio.h>
 
     bool inited = false;
 
@@ -13,9 +14,19 @@ volatile void *___ = NULL;
         const WORD version = MAKEWORD(2, 2);
 
         WSADATA data;
-        if (WSAStartup(version, &data)) abort();
+        if (int err = WSAStartup(version, &data))
+        {
+            fprintf(stderr, "[libsocket]: WSAStartup error %i. Application aborted.\n", err);
+            abort();
+        }
 
-        if (data.wVersion != version) { WSACleanup(); abort(); }
+        if (data.wVersion != version)
+        {
+            fprintf(stderr, "[libsocket]: responced WinSock version (high.low: %u.%u) doesn't match requested version (high.low: %u.%u). Application aborted.\n", HIBYTE(data.wVersion), LOBYTE(data.wVersion), HIBYTE(version), LOBYTE(version));
+
+            WSACleanup();
+            abort();
+        }
 
         inited = true;
     }
